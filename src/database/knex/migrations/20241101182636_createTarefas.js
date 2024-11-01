61d4ca455fbadf874d79de3f0dff502c0366bd12
Vinícius <santsmarcos81@gmail.com>
2024-11-01 15:50:55 -0300

    Criando migrates e configurando banco de dados com knex

---
+++ src/database/knex/migrations/20241101182636_createTarefas.js
@@ -0,0 +1,9 @@
+exports.up = knex => knex.schema.createTable("Tarefas", table => {
+  table.increments("identificador_da_tarefa");
+  table.text("nome_da_tarefa").notNullable();
+  table.integer("custo").notNullable();
+  table.timestamp("data_limite").notNullable();
+  table.integer("ordem_da_apresentacao");
+});
+
+exports.down = knex => knex.schema.dropTable("Tarefas");
